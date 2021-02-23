89b7c83e3b3a636999494dcfbc41a4bd16ad65a0
x <x@x.com>
2021-02-23 21:20:13 +0800

    160 intersection-of-two-linked-lists

---
+++ 160/Solution.java
@@ -0,0 +1,10 @@
+public class Solution {
+	public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+		ListNode n1 = headA, n2 = headB;
+		while (n1 != n2) {
+			n1 = n1 == null ? headB : n1.next;
+			n2 = n2 == null ? headA : n2.next;
+		}
+		return n1; // 若不相交，n1, n2在走完A+B后都会走到null
+	}
+}
