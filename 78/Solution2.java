261402d52f453fd8187004c160b490cb0caa01d1
x <x@x.com>
2021-03-08 22:48:19 +0800

    78 subsets

---
+++ 78/Solution2.java
@@ -0,0 +1,23 @@
+import java.util.LinkedList;
+import java.util.List;
+
+class Solution {
+	private List<List<Integer>> ans;
+
+	public List<List<Integer>> subsets(int[] nums) {
+		ans = new LinkedList<>();
+		recur(nums, 0, new LinkedList<Integer>());
+		return ans;
+	}
+
+	private void recur(int[] nums, int lo, LinkedList<Integer> list) {
+		ans.add(new LinkedList<Integer>(list));
+		// append the effective nums to the list one by one
+		// len(list)++
+		for (int i = lo; i < nums.length; i++) {
+			list.add(nums[i]);
+			recur(nums, i + 1, list);
+			list.removeLast();
+		}
+	}
+}
