87c6432147bba2343020caca665d0759ed1cf8e9
x <x@x.com>
2021-03-08 15:04:53 +0800

    139 word-break: improve

--- 139/Solution.java
+++ 139/Solution.java
@@ -5,8 +5,10 @@ class Solution {
 		boolean[] dp = new boolean[s.length() + 1];
 		dp[0] = true;
 		for (int i = 1; i < dp.length; i++) { // current char is s.charAt(i - 1)
+			// column:	dp[i][j] = dp[i][j - 1] || (substr.equals(wj) && dp[i - wj.length()][j])
 			for (String w : wordDict) {
-				if (i >= w.length() && dp[i - w.length()] && s.substring(i - w.length(), i).equals(w)) dp[i] = true;
+				//if (i >= w.length() && dp[i - w.length()] && s.substring(i - w.length(), i).equals(w)) dp[i] = true;
+				if (i >= w.length()) dp[i] = dp[i] || (s.substring(i - w.length(), i).equals(w) && dp[i - w.length()]);
 			}
 		}
 		return dp[dp.length - 1];
