cb5a601028981f74c0fcc93010e5f53c16f2f747
azjf <a@z.jf>
2021-03-16 14:51:45 +0800

    215 kth-largest-element-in-an-array

---
+++ 215/Solution2.java
@@ -0,0 +1,12 @@
+import java.util.PriorityQueue;
+
+class Solution {
+	public int findKthLargest(int[] nums, int k) {
+		PriorityQueue<Integer> pq = new PriorityQueue<>(k);
+		for (int num : nums) {
+			pq.offer(num);
+			if (pq.size() > k) pq.poll();
+		}
+		return pq.poll();
+	}
+}
