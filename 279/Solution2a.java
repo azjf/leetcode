27c0e146aeb9782dc7415b6d6393b664bfcfbd44
x <x@x.com>
2021-03-24 23:05:05 +0800

    279 perfect-squares: improve

--- 279/Solution2a.java
+++ 279/Solution2a.java
@@ -1,14 +1,14 @@
 class Solution {
 	public int numSquares(int n) {
 		int[] dp = new int[n + 1];
-		for (int j = 0; j < dp.length; j++) dp[j] = n + 1;
+		for (int i = 0; i < dp.length; i++) dp[i] = n + 1;
 		dp[0] = 0;
 
 		for (int i = 1; i < dp.length; i++) {
 			// row: 	dp[i][j] = min(dp[i - 1][j], 1 + dp[i][j - i * i])  // i (item), j (space), dp[i][j] (value)
 			// column:	dp[i][j] = min(dp[i][j - 1], 1 + dp[i - j * j][j])  // i (space), j (item), dp[i][j] (value)
-			for (int j = i * i; j < dp.length; j++) {  // row
-				dp[j] = Math.min(dp[j], 1 + dp[j - i * i]);
+			for (int j = 1; j * j <= i; j++) {  // column
+				dp[i] = Math.min(dp[i], 1 + dp[i - j * j]);
 			}
 		}
 		return dp[dp.length - 1];
--- 279/Solution2b.java
+++ 279/Solution2b.java
@@ -13,10 +13,10 @@ class Solution {
 
 		int[] dp = new int[n + 1];
 		for (int i = 1; i < dp.length; i++) {
-			// row: 	dp[i][j] = min(dp[i - 1][j], dp[i][i - j * j])
-			// column:	dp[i][j] = min(dp[i][j - 1], dp[i - j * j][j])
+			// row: 	dp[i][j] = min(dp[i - 1][j], 1 + dp[i][j - i * i])  // i (item), j (space), dp[i][j] (value)
+			// column:	dp[i][j] = min(dp[i][j - 1], 1 + dp[i - j * j][j])  // i (space), j (item), dp[i][j] (value)
 			int min = n + 1;
-			for (int s : squares) {
+			for (int s : squares) {  // column
 				if (s > i) break;
 				min = Math.min(min, 1 + dp[i - s]);
 			}
