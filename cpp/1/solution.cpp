e689ca4f45ce515d6326850b47fd8966ffa53fc8
azjf <a@z.jf>
2021-04-07 20:45:29 +0800

    cpp: 1 two-sum

---
+++ cpp/1/solution.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+
+class Solution {
+    public:
+        vector<int> twoSum(vector<int>& nums, int target) {
+            unordered_map<int, int> map;
+            for (int i = 0; i < nums.size(); ++i) {
+                int x = nums[i], y = target - nums[i];
+                if (map.count(y)) return { i, map[y] };
+                map[x] = i;
+            }
+            return {};
+        }
+};
