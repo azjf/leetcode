7d92ddd616652bd4d18b47ed36d1b2aca042ed52
x <x@x.com>
2021-04-07 23:27:22 +0800

    cpp: 104 maximum-depth-of-binary-tree

---
+++ cpp/104/solution.cpp
@@ -0,0 +1,25 @@
+#include <algorithm>
+
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+
+class Solution {
+    public:
+        int maxDepth(TreeNode *root) {
+            return recur(root);
+        }
+
+    private:
+        int recur(TreeNode *cur) {
+            if (!cur) return 0;
+            return 1 + std::max(recur(cur->left), recur(cur->right));
+        }
+};
