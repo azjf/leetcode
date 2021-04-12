1fdb7fa106df7f85289fecf61d818df6e6eae196
x <x@x.com>
2021-04-12 15:27:44 +0800

    cpp: 191 number-of-1-bits

---
+++ cpp/191/solution.cpp
@@ -0,0 +1,14 @@
+#include <cstdint>
+
+
+class Solution {
+    public:
+        int hammingWeight(uint32_t n) {
+            int ans = 0;
+            while (n) {
+                ++ans;
+                n &= n - 1;
+            }
+            return ans;
+        }
+};
