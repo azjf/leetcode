d2b9a9b4891a42d079481bc9002f7ee397f6ca1a
x <x@x.com>
2021-04-12 19:55:04 +0800

    cpp: 219 contains-duplicate-ii

---
+++ cpp/219/solution.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+
+class Solution {
+    public:
+        bool containsNearbyDuplicate(vector<int>& nums, int k) {
+            unordered_map<int, int> map;
+            for (int i = 0; i < nums.size(); ++i) {
+                if (map.count(nums[i]) && map[nums[i]] >= i - k) return true;
+                map[nums[i]] = i;
+            }
+            return false;
+        }
+};
