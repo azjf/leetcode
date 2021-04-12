f39eed2d81bb609d2b109bbeb62a5638eb1676d9
azjf <a@z.jf>
2021-04-12 16:56:41 +0800

    cpp: 198 house-robber

---
+++ cpp/198/solution.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+
+using namespace std;
+
+
+class Solution {
+    public:
+        int rob(vector<int>& nums) {
+            // dp[i] = max(dp[i - 1], nums[i] + dp[i - 2])
+            int pre = 0, ppre = 0;
+            for (int num : nums) {
+                int cur = max(pre, ppre + num);
+                ppre = pre;
+                pre = cur;
+            }
+            return pre;
+        }
+};
