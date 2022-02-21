0a1a2afcaeb30a414e0904494a21334653cd7b66
azjf <a@z.jf>
2022-02-21 19:12:56 +0800

    121 best-time-to-buy-and-sell-stock-ii

---
+++ 122/Solution.h
@@ -0,0 +1,17 @@
+#include <vector>
+#include <unordered_map>
+
+class Solution
+{
+public:
+    int maxProfit(std::vector<int> &prices)
+    {
+        int profit = 0;
+        for (decltype(prices.size()) i = 1; i < prices.size(); ++i) {
+            if (prices[i] > prices[i - 1]) {
+                profit += prices[i] - prices[i - 1];
+            }
+        }
+        return profit;
+    }
+};
