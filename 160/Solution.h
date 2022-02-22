279f1f12d24798e1a8aa8ff1f3d70036092393f6
azjf <a@z.jf>
2022-02-22 14:28:57 +0800

    160 intersection-of-two-linked-lists

---
+++ 160/Solution.h
@@ -0,0 +1,15 @@
+#include "ListNode.h"
+
+class Solution
+{
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB)
+    {
+        ListNode *n1 = headA, *n2 = headB;
+        while (n1 != n2) {
+            n1 = n1 ? n1->next : headB;
+            n2 = n2 ? n2->next : headA;
+        }
+        return n1;
+    }
+};
