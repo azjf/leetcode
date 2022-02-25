df0a2158d888d1801226967e94c19aa3103a9875
azjf <a@z.jf>
2022-02-25 20:39:07 +0800

    821 shortest-distance-to-a-character

---
+++ 821/Solution.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+class Solution
+{
+public:
+    std::vector<int> shortestToChar(std::string s, char c)
+    {
+        std::vector<int> ret(s.size(), -10000);
+        int l = -s.size();
+        for (decltype(s.size()) i = 0; i != s.size(); ++i) {
+            if (s[i] == c) {
+                l = i;
+            }
+            ret[i] = i - l;
+        }
+        int r = s.size() * 2;
+        for (decltype(s.size()) i = s.size() - 1; i != -1; --i) {
+            if (s[i] == c) {
+                r = i;
+            }
+            ret[i] = std::min(ret[i], r - (int) i);
+        }
+        return ret;
+    }
+};
