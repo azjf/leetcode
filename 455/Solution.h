ea772e5bbca89626dcad625ef5cfd7f0847a1238
azjf <a@z.jf>
2022-02-25 16:32:41 +0800

    455 assign-cookies

---
+++ 455/Solution.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <vector>
+#include <algorithm>
+
+class Solution
+{
+public:
+    int findContentChildren(std::vector<int> &g, std::vector<int> &s)
+    {
+        std::sort(g.begin(), g.end());
+        std::sort(s.begin(), s.end());
+
+        int ans = 0;
+        auto it_g = g.cbegin(), it_s = s.cbegin();
+        while (it_g != g.cend() && it_s != s.cend()) {
+            if (*it_g <= *it_s) {
+                ++ans;
+                ++it_g;
+            }
+            ++it_s;
+        }
+        return ans;
+    }
+};
